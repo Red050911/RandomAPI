b5cbd3c96040c1b966ba7fc4058ffee7d217dff2
Jacob Rutledge <jacob.rutledge0509@gmail.com>
2022-06-16 18:25:12 -0500

    Initial commit

---
+++ src/main/java/io/github/red050911/plugin/randomapi/RandomAPI.java
@@ -0,0 +1,18 @@
+package io.github.red050911.plugin.randomapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    
+}
